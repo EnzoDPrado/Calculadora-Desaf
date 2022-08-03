e9365969abe0b13ec16f133262d88e6846bada3f
Enzo D. Prado <84857077+EnzoDPrado@users.noreply.github.com>
2022-08-03 07:59:39 -0300

    Add files via upload

--- brinks/app.js
+++ brinks/app.js
@@ -1,15 +1,54 @@
 'use script'
 
-function juntarNome (){
-    const nome = document.getElementById('nome').value;
-    const sobreNome = document.getElementById('sobreNome').value;
+function opSoma (){
+    const valor1soma = document.getElementById('valor1soma').value;
+    const valor2soma = document.getElementById('valor2soma').value;
     const resultado = document.getElementById('resultado')
     
-    certo = parseFloat(nome) + parseFloat(sobreNome);
-    nomeCompleto = `O valor da soma é: ${certo}` ;
-    console.log(nomeCompleto);
+    certo = parseFloat(valor1soma) + parseFloat(valor2soma);
+    valorReal = `O valor da soma é: ${certo}` ;
+    console.log(valorReal);
 
-    resultado.textContent = nomeCompleto;
+    resultado.textContent = valorReal;
 }
 
-document.getElementById('juntar').addEventListener('click', juntarNome)
+function opSubtracao(){
+    const valor1soma = document.getElementById('valor1soma').value;
+    const valor2soma = document.getElementById('valor2soma').value;
+    const resultado = document.getElementById('resultado')
+    
+    certo = parseFloat(valor1soma) - parseFloat(valor2soma);
+    valorReal = `O valor da subtracao é: ${certo}` ;
+    console.log(valorReal);
+
+    resultado.textContent = valorReal;
+}
+
+function opMultiplicacao(){
+    const valor1soma = document.getElementById('valor1soma').value;
+    const valor2soma = document.getElementById('valor2soma').value;
+    const resultado = document.getElementById('resultado')
+    
+    certo = parseFloat(valor1soma) * parseFloat(valor2soma);
+    valorReal = `O valor da multiplicacao é: ${certo}` ;
+    console.log(valorReal);
+
+    resultado.textContent = valorReal;
+}
+
+function opDivisao(){
+    const valor1soma = document.getElementById('valor1soma').value;
+    const valor2soma = document.getElementById('valor2soma').value;
+    const resultado = document.getElementById('resultado')
+    
+    certo = parseFloat(valor1soma) / parseFloat(valor2soma);
+    valorReal = `O valor da divisao é: ${certo}` ;
+    console.log(valorReal);
+
+    resultado.textContent = valorReal;
+}
+
+document.getElementById('juntarSoma').addEventListener('click', opSoma);
+document.getElementById('juntarSubtracao').addEventListener('click', opSubtracao);
+document.getElementById('juntarMultiplicar').addEventListener('click', opMultiplicacao);
+document.getElementById('juntarDividir').addEventListener('click', opDivisao)
